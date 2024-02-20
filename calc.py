a8817191c8255a2ee189bf02541e694dc2b9a826
Anil Kadurka <anilkadurka@gmail.com>
2024-02-20 13:36:07 +0530

    added sub.py

--- calc.py
+++ calc.py
@@ -1,7 +1,11 @@
 from addition import addition
+from substraction import substraction
 
 print("This is Calculator App")
 
 
 x=addition(1,2)
 print("The sum is ",x)
+
+x=substraction(1,2)
+print("The sub is ",x)
